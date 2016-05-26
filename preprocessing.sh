864ddcb65803091ed4a6058e6da394929cab45ef
Frank <zhangc518@gmail.com>
2016-05-26 16:53:55 -0700

    updated the preprossing

--- preprocessing.sh
+++ preprocessing.sh
@@ -2,7 +2,7 @@
 echo -e "############################################################"
 echo -e "(`date`) initating input parameters ....."
 echo -e "############################################################"
-
+# count 
 #input 1: consolidated FASTQ Folder 
 
 PARENT_DIR=$PWD #  project dir
@@ -28,7 +28,7 @@ echo -e " total: $TOTAL_PROC_NO processors will be using for this analysis \n" |
 
 echo -e "Please input the password info (example: )\n" | tee -a $LOG_FILE
 PASSWD_INFO_INPUT="SxaQSEQsWA148L3:wa4kD9Ye8hr8"
-
+echo -e "Your password is : $PASSWD_INFO_INPUT"
 
 echo -e "############################################################"| tee -a $LOG_FILE
 echo -e "`date` Running the pipelines " | tee -a $LOG_FILE
@@ -40,26 +40,46 @@ echo -e "0.1 `date` starting downloading" | tee -a $LOG_FILE
 echo -e "############################################################"| tee -a $LOG_FILE
 
 STEP="00raw"; WORK_DIR=$PARENT_DIR"/"$STEP
-mkdir $WORK_DIR; cd $WORK_DIR
+mkdir -p $WORK_DIR; cd $WORK_DIR
 
-grab_bscrc.sh SxaQSEQsWA148L3:wa4kD9Ye8hr8 # why freeze here
+#grab_bscrc.sh $PASSWD_INFO_INPUT
+
 wait;echo -e "(`date`) downloaded the raw qseq data" | tee -a $LOG_FILE
+RAW_DIR=$(echo $PASSWD_INFO_INPUT|cut -f1 -d":"); RAW_DIR=$WORK_DIR/$RAW_DIR; cd $RAW_DIR
+echo -e "(`date`) there are total: `ls -1 | wc -l`  raw qseq data" | tee -a $LOG_FILE
 
 echo -e "############################################################"| tee -a $LOG_FILE
 echo -e "0.2 `date` starting decompressing " | tee -a $LOG_FILE
 echo -e "############################################################"| tee -a $LOG_FILE
 
-RAW_DIR=$(echo $PASSWD_INFO_INPUT|cut -f1 -d":"); RAW_DIR=$WORK_DIR/$RAW_DIR; cd $RAW_DIR
-ls -1 *.gz | parallel -j $TOTAL_PROC_NO --eta gunzip 
+
+#ls -1 *.gz | parallel -j $TOTAL_PROC_NO --eta gunzip 
 wait;echo -e "(`date`) decompressed the raw qseq data" | tee -a $LOG_FILE
 
 
 echo -e "############################################################"| tee -a $LOG_FILE
 echo -e "0.3 `date` starting converting qseq to fastq " | tee -a $LOG_FILE
 echo -e "############################################################"| tee -a $LOG_FILE
+#echo $PWD
+#ls -1 * | while read data; do  echo ${data:0:10}; done | tee -a $LOG_FILE
 
-ls -1 * | while read data; do  echo ${data:0:10}; done | tee -a $LOG_FILE
-ls -1 * | while read data; do  qseq2fastq.pl $data ${data:0:10}'.fastq' & done | tee -a $LOG_FILE
+qseq2fastqPar (){
+    count=1;ncore=$TOTAL_PROC_NO;
+    prefix=$WORK_DIR"/"
+    while read data
+    do
+
+        if [ `echo $count" % "$ncore | bc` -eq 0 ] 
+        then
+	    qseq2fastq.pl $data ${data:0:10}'.fastq' 
+        else
+	    qseq2fastq.pl $data ${data:0:10}'.fastq' &
+        fi
+        count=$((count+1))
+    done
+}
+
+#ls -1 * | qseq2fastqPar | tee -a $LOG_FILE
 wait;echo -e "(`date`) 0.3 convering finished" | tee -a $LOG_FILE
 
 echo -e "############################################################"| tee -a $LOG_FILE
@@ -67,10 +87,11 @@ echo -e "0.4 `date` starting demultiplex " | tee -a $LOG_FILE
 echo -e "############################################################"| tee -a $LOG_FILE
 
 STEP="consolidate";
-WORK_DIR=$WORK_DIR'/'$STEP; mkdir $WORK_DIR
+WORK_DIR=$WORK_DIR'/'$STEP; mkdir -p $WORK_DIR
 
 demultiplexFunMuticore (){
-    count=1;ncore=$TOTAL_PROC_NO;
+    count=1
+    ncore=$TOTAL_PROC_NO;
     prefix=$WORK_DIR"/"
     while read data
     do
@@ -79,17 +100,20 @@ demultiplexFunMuticore (){
         idxfile=$(echo "$data" |sed -r 's/_1_/_2_/')
         echo "using idx file: "$idxfile
         echo "calulating no.$count sampling ......"
+	echo "suffix is $suffix"
+	echo "save into folder $prefix"
+	echo "(`date`) $count"
         if [ `echo $count" % "$ncore | bc` -eq 0 ] #mod ncore
         then
-            cat $data | fastx_barcode_splitter.pl --bcfile $BARCODE_FILE --prefix $prefix --suffix $suffix --idxfile $idxfile --mismatches 1
-        else
-            cat $data | fastx_barcode_splitter.pl --bcfile $barcodefile --prefix $prefix --suffix $suffix --idxfile $idxfile --mismatches 1 &
+	    cat $data | fastx_barcode_splitter.pl --bcfile $BARCODE_FILE --prefix $prefix --suffix $suffix --idxfile $idxfile --mismatches 1
+       else
+           cat $data | fastx_barcode_splitter.pl --bcfile $BARCODE_FILE --prefix $prefix --suffix $suffix --idxfile $idxfile --mismatches 1 &
         fi
         count=$((count+1))
     done
 }
 
-ls -1 *_1_*.fastq | demultiplexFunMuticore 1>>$LOG_FILE 2>>$LOG_ERR_FILE
+#ls -1 *_1_*.fastq | demultiplexFunMuticore 1|tee -a $LOG_FILE 2>>$LOG_ERR_FILE
 
 wait;echo -e "(`date`) 0.4 demultiplex finished" | tee -a $LOG_FILE
 
@@ -100,11 +124,8 @@ echo -e "############################################################"| tee -a $
 cd $WORK_DIR
 while read line; do
     prefix=$(echo -e "$line" | cut -f1 -d$'\t')
-    #eval "ls ${prefix}* "
-    echo $prefix
-    eval "cat ${prefix}* > ${prefix}.fastq && rm ${prefix}_* &"
-    #eval "rm ${prefix}_* " 
-done<$BARCODEFILE | tee -a $LOG_FILE
+    eval "cat ${prefix}_* > ${prefix}.fastq && rm ${prefix}_* &"
+done<$BARCODE_FILE | tee -a $LOG_FILE
 wait;echo -e "(`date`) 0.5 demultiplex finished" | tee -a $LOG_FILE
 
 
